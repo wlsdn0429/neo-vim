1323874bc52b9d684b577382cea7e3ef12d81d16
jwlee <wlsdn0429@naver.com>
2024-07-06 23:54:49 +0900

    최초 neo-vim config 업로드

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+local mapKey = require("utils.keyMapper").mapKey
+
+mapKey('<leader>e', ':Neotree toggle<cr>')
+
+mapKey('<C-h>', '<C-w>h')
+mapKey('<C-j>', '<C-w>j')
+mapKey('<C-k>', '<C-w>k')
+mapKey('<C-l>', '<C-w>l')
+
+mapKey('<leader>h', ':nohlsearch<CR>')
